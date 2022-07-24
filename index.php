27eeae17add3ee28a2e3b0c6d4535d3db32761ba
trent <trent.smith0115@gmail.com>
2022-07-24 17:43:54 -0400

    Added my new file.

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+
+//Common git commands
 //git init (creates new git repo in current directory)
 //git status
 //git add . 
 //git commit -m "message for the commit"
+
+//Create new branches
+// git checkout -b branch-name
 
 echo "<h1>Hello World!</h1>";
 
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo "<h2>This is a file for my new feature.</h2>";
+
+?>
